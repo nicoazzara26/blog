d44adeaf7f2377f1cba3c26daebf0bbe0a62f30a
Nicolas Azzara <nicolas.azzara@globalogic.com>
2016-05-05 17:37:37 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 var menu;
-var btnCerrar;
+
 
 window.onload = function() {
 
 menu = document.getElementById('MainMenu');
-btnCerrar = document.getElementById('BtnCerrar')
+
  }
 
   function cambiarEstilo(propiedad, valor) {
                                   menu.style[propiedad] = valor;
-                                  BtnCerrar.style[propiedad] = valor;
                               }
-      
